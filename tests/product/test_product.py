e4d1e39cc08e295fa53beb0199edcdce1a590047
Italo <contatoitallo@hotmail.com>
2022-10-28 15:50:12 -0300

    test: Implementa teste para verificar se  o método __init__ da classe Product esta funcionando corretamente

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    result = Product(
+        1,
+        "t",
+        "t",
+        "t",
+        "t",
+        "t",
+        "t",
+    )
+
+    assert result.id == 1
+    assert result.nome_do_produto == "t"
+    assert result.nome_da_empresa == "t"
+    assert result.data_de_fabricacao == "t"
+    assert result.data_de_validade == "t"
+    assert result.numero_de_serie == "t"
+    assert result.instrucoes_de_armazenamento == "t"
