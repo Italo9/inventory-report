e50d426e6581e1634e6e670e61d66692c88b7e93
Italo <contatoitallo@hotmail.com>
2022-10-31 08:22:37 -0300

    feat: :sparkles: Implementa teste da função que gera relatório de produto para validar a conformidade da frase de retorno

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    result = Product(
+        id=1,
+        nome_do_produto="cadeira",
+        nome_da_empresa="Target Corporation",
+        data_de_fabricacao="2021-02-18",
+        data_de_validade="2025-09-17",
+        numero_de_serie="CR25",
+        instrucoes_de_armazenamento="empilhadas",
+    )
+
+    assert (
+        str(result) == f"O produto {result.nome_do_produto}"
+        f" fabricado em {result.data_de_fabricacao}"
+        f" por {result.nome_da_empresa} com validade"
+        f" até {result.data_de_validade}"
+        f" precisa ser armazenado {result.instrucoes_de_armazenamento}."
+    )
