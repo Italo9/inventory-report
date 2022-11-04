0e55f027da73ffd5adcb43f40eb2c924eac8bed3
Italo <contatoitallo@hotmail.com>
2022-11-04 10:09:53 -0300

    feat: :sparkles: Altera o método import_data de tal forma que seja possível importar um arquivo json, retorna o relatório simples ou o completo conforme solicitado

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
@@ -7,12 +8,16 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @staticmethod
     def import_data(string_path, string_type):
-        with open(string_path, encoding="utf-8") as file:
-            result = csv.DictReader(file, delimiter=",", quotechar='"')
-            result_products = []
-            for row in result:
-                result_products.append(row)
+        if string_path.endswith(".csv"):
+            with open(string_path, encoding="utf-8") as file:
+                result = csv.DictReader(file, delimiter=",", quotechar='"')
+                result_products = []
+                for row in result:
+                    result_products.append(row)
+        elif string_path.endswith(".json"):
+            with open(string_path) as file:
+                result_products = json.load(file)
         if string_type == "simples":
             return SimpleReport.generate(result_products)
-        if string_type == "completo":
+        else:
             return CompleteReport.generate(result_products)
